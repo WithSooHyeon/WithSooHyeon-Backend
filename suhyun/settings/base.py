e1bd0fbdeb78a95fa7e902a6374b0d8e4b3ac666
Yaeran Kim <yaerankim000812@gmail.com>
2023-11-28 02:08:06 +0900

    settings, requirements 분리

--- suhyun/settings/base.py
+++ suhyun/settings/base.py
@@ -11,36 +11,54 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+secret_file = BASE_DIR / 'secrets.json'
+with open(secret_file) as file: # secrets파일 읽어와서 json으로 처리 
+    secrets = json.loads(file.read())
+    
+# secret 가져오다가 생기는 오류 확인
+def get_secret(setting, secrets_dict=secrets):
+    try:
+        return secrets_dict[setting]
+    except KeyError: # key가 없는 경우
+        error_msg = f'Set the {setting} environment variable'
+        raise ImproperlyConfigured(error_msg)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h%v#k1@m)g9_4d3*vk1t)t(sk44j-nui2mxa5w=e5bp-_9k_xg'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = get_secret('SECRET_KEY')
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+PROJECT_APPS =[
     'rest_framework',
     'counsel',
     'findsh',
 ]
+
+THIRD_PARTY_APPS = [
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -71,17 +89,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'suhyun.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
